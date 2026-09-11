a906dab1cd49d9c00a42d97f6fcd9c4e4e3a98db
Masud Rana <rannamasudrana753@gmail.com>
2026-09-12 00:56:24 +0600

    WIP:

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import httpStatus from "http-status";
-import { Role } from "../../generated/prisma/enums";
+import { DoctorVerificationStatus, Role } from "../../generated/prisma/enums";
 import config from "../config";
 import { prisma } from "../lib/prisma";
 import { AppError } from "./AppError";
@@ -158,6 +158,7 @@ export const seedTesterDoctor = async () => {
             licenseNumber: "BMDC0000",
             qualifications: "MBBS",
             specialization: "Neurology",
+            verificationStatus: DoctorVerificationStatus.APPROVED,
           },
         },
       },
